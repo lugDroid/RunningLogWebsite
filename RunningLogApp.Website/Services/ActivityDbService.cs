dc39bdadf084c436a76fe33d6b3a5b50d95b8c0d
lugDroid <diazbastos@gmail.com>
2020-01-29 08:57:09 +0100

    Add code to update athlete properties in database

--- RunningLogApp.Website/Services/ActivityDbService.cs
+++ RunningLogApp.Website/Services/ActivityDbService.cs
@@ -39,8 +39,18 @@ namespace RunningLogApp.Website.Services
         {
             var existingAthletes = await ReadAthleteDataAsync();
 
-            // TO-DO: Add code to update existing athlete data with new info
-            if (!existingAthletes.Any(x => x.Id == athlete.Id))
+            if (existingAthletes.Any(x => x.Id == athlete.Id))
+            {
+                var existingAthlete = existingAthletes.First<Athlete>(x => x.Id == athlete.Id);
+
+                if (existingAthlete != null)
+                {
+                    CopyProperties(athlete, existingAthlete);
+
+                    _context.Athlete.Update(existingAthlete);
+                }
+            }
+            else
             {
                 _context.Athlete.Add(athlete);
             }
@@ -81,6 +91,25 @@ namespace RunningLogApp.Website.Services
         public Task<MonthlySummary[]> ReadMonthlySummariesAsync()
         {
             return Task.FromResult(_context.MonthlySummaries.ToArray());
+        }
+
+        private void CopyProperties<T,TU>(T source, TU dest)
+        {
+            var sourceProps = typeof(T).GetProperties().Where(x => x.CanRead).ToList();
+            var destProps = typeof(TU).GetProperties().Where(x => x.CanWrite).ToList();
+
+            foreach (var sourceProp in sourceProps)
+            {
+                if (destProps.Any(x => x.Name == sourceProp.Name))
+                {
+                    var property = destProps.First(x => x.Name == sourceProp.Name);
+
+                    if (property.CanWrite)
+                    {
+                        property.SetValue(dest, sourceProp.GetValue(source, null), null);
+                    }
+                }
+            }
         }
     }
 }
